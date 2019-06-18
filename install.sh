ba3ad5dfb9514629ba394a73901162c4d78de182
Josef Matondang <josstemat@gmail.com>
2019-06-18 09:35:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,22 +3,27 @@
 DIR=`dirname $0`
 
 # Install the dependencies
+echo "Updating and installing the dependencies"
 /usr/bin/apt update
 /usr/bin/apt -y upgrade
 /usr/bin/apt install -y bluez-tools
 
 # Copy the services to systemd
+echo "Copying the bluetooth agent service to systemd"
 /bin/cp "${DIR}/bluetooth.service" /lib/systemd/system/bluetooth.service
 /bin/cp "${DIR}/blue-agent.service" /etc/systemd/system/blue-agent.service
 
 # Add pi to the bluetooth user group
+echo "Adding the PI user account to bluetooth system group"
 /usr/sbin/adduser pi bluetooth
 
 # Copy the PIN configuration file
+echo "Copying the bluetooth pin configuration file"
 /bin/cp "${DIR}/pin.conf" /etc/bluetooth/pin.conf
 /bin/chmod 600 /etc/bluetooth/pin.conf
 
 # Reload the systemd daemon and add the services
+echo "Restarting the systemd daemon"
 /bin/systemctl daemon-reload
 /bin/systemctl enable blue-agent.service
 /bin/systemctl start blue-agent.service
